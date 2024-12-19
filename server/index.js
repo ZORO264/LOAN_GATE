1216e04c3382a8b3b57278af46fcaedea024ffcf
ABHI RAM <ramabhi317@gmail.com>
2024-12-19 16:15:58 +0530

    Initial project setup with backend

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch(err=>{
+    console.log("MongoDB connection error:",err);
+});
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to Loan Gate Backend!");
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+});
+
